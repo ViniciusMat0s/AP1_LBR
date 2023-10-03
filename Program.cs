3c0616db4479a91407b00c58f62af31e08f0e99f
ViniciusMat0s <vinibmatos@rede.ulbra.br>
2023-10-03 17:33:39 -0300

    1111

--- Program.cs
+++ Program.cs
@@ -1,33 +1,36 @@
-﻿string continuar;
+﻿string continuar = "0";
 do {
     //Coletando os dados:
     Console.Write("Digite seu nome: ");
-    string nome = Console.ReadLine();
+    string nome = (Console.ReadLine());
+
     Console.Write("Digite sua idade: ");
-    int idade = Convert.ToInt32(Console.ReadLine);
-    Console.Write("Digite sua altura:");
+    int idade = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Digite sua altura: ");
     double altura = Convert.ToDouble(Console.ReadLine());
+
     Console.Write("Digite seu peso: ");
     double peso = Convert.ToDouble(Console.ReadLine());
 
     //Calculando IMC
     double imc = peso / (altura * altura);
 
-    string faixaEtaria;
-    if (idade >= 0 && idade >= 11) {
+    string faixaEtaria = "0";
+    if (idade >= 0 && idade <= 11) {
         faixaEtaria = "Criança";
     }
-    else if (idade >= 12 && idade >= 17) {
+    else if (idade >= 12 && idade <= 17) {
         faixaEtaria = "Adolescente";
     }
-    else if (idade >= 18 && idade >= 59) {
+    else if (idade >= 18 && idade <= 59) {
         faixaEtaria = "Adulto";
     }
     else if (idade >= 60) {
         faixaEtaria = "Idoso";
     }
 
-    string imcInfo;
+    string imcInfo = "0";
     if (imc < 18.5) {
         imcInfo = "Abaixo do peso";
     }
@@ -48,13 +51,17 @@ do {
     }
 
     //Relatório
-    Console.WriteLine("\n =-=-=-=-=-=-=-= Informações =-=-=-=-=-=-=-=");
-    Console.WriteLine($"Nome: {nome}\n");
-    Console.WriteLine($"Idade: {idade}\n");
-    Console.WriteLine("\nFaixa etária: " + faixaEtaria);
-    Console.WriteLine("\nIMC: " + imc);
-    Console.WriteLine("\nStatus: " + imcInfo);
-    Console.Write("\nDeseja continuar (s/n)? ");
+    Console.WriteLine("\n =-=-=-=-=-=-= Informações =-=-=-=-=-=-=");
+    Console.WriteLine($"\tNome: {nome}\n");
+    Console.WriteLine($"\tIdade: {idade}\n");
+    Console.WriteLine("\tFaixa etária: " + faixaEtaria);
+    Console.WriteLine("\tIMC: " + imc);
+    Console.WriteLine("\tStatus: " + imcInfo);
+    Console.Write("\nDeseja continuar (s/n)?\n");
+    continuar = Console.ReadLine().ToLower();
+    Console.WriteLine("\n =-=-=-=-=-=-= =---------= =-=-=-=-=-=-=");
+
 } while (continuar.ToLower() == "s");
 
-Console.WriteLine("Finalizando programa.");
+Console.WriteLine("\tFinalizando programa.");
+Console.WriteLine("\n =-=-=-=-=-=-= =---------= =-=-=-=-=-=-=");
